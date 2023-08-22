e9d8f4c5cbb272e838ddb06028e21554bdc6bd1e
XiaoZheng2003 <1345486584@qq.com>
2023-08-22 18:52:31 +0800

    主界面添加群聊功能

--- Qt-IM/mainwindow.cpp
+++ Qt-IM/mainwindow.cpp
@@ -10,18 +10,24 @@ MainWindow::MainWindow(QWidget *parent) :
 
     ui->ipAddress->setText("IP地址："+NetworkTool::GetLocalIP());
 
+    //单聊页面
     QTreeWidget *personList=ui->personList;
     personList->setColumnWidth(0,150);
     personList->setIconSize(QSize(25,25));
-
     //分组
-    QTreeWidgetItem *group=new QTreeWidgetItem(personList);
-    group->setText(0,"分组1");
-
+    QTreeWidgetItem *tag=new QTreeWidgetItem(personList);
+    tag->setText(0,"分组1");
     //用户
-    QTreeWidgetItem *person=new QTreeWidgetItem(group);
+    QTreeWidgetItem *person=new QTreeWidgetItem(tag);
     person->setText(0,"用户1");
     person->setIcon(0,QIcon(":/res/avatar.jpg"));
+
+    //群聊页面
+    QTreeWidget *groupList=ui->groupList;
+    groupList->setIconSize(QSize(25,25));
+    QTreeWidgetItem *group=new QTreeWidgetItem(groupList);
+    group->setText(0,"群聊1");
+    group->setIcon(0,QIcon(":/res/group.png"));
 
 }
 
