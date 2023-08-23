da6f989c54f00e38f1aae3b683ccd856ff133e51
Hihixiaolv <3181871180@qq.com>
2023-08-23 08:59:16 +0000

    !11 修改传输文件页面

--- Qt-IM/file.cpp
+++ Qt-IM/file.cpp
@@ -1,14 +1,45 @@
 #include "file.h"
 #include "ui_file.h"
+#include <QFileDialog>
+#include <QMessageBox>
 
-file::file(QWidget *parent) :
+File::File(QWidget *parent) :
     QWidget(parent),
-    ui(new Ui::file)
+    ui(new Ui::File)
 {
     ui->setupUi(this);
 }
 
-file::~file()
+File::~File()
 {
     delete ui;
 }
+
+void File::on_Button_choose_clicked()
+{
+    QString path = QFileDialog::getOpenFileName(this,tr("选择一个文件"),".",tr("All Files(*.*)"));
+    if(!path.isEmpty())
+    {
+        QFile file(path);
+        if(!file.open(QIODevice::ReadOnly))
+        {
+            QMessageBox::warning(this,tr("打开文件"),
+            tr("打开文件失败:\n%1").arg(path));
+            return;
+        }
+        QTextStream in(&file);
+        fname=path;
+        if(!fname.isEmpty())
+        {
+            QMessageBox::information(this,tr("信息"),tr("选择文件成功."));
+            QStringList tmp=fname.split('/');
+            ui->file->setText(tmp[tmp.length()-1]);
+        }
+        //ui->textEdit->setText(in.readAll());
+        file.close();
+
+    }else {
+        QMessageBox::warning(this,tr("Path"),tr("未选择文件."));
+    }
+}
+
