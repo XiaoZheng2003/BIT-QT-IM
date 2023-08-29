0b06723a13e36157d962f1d4249765bec19498e1
cc1110 <1790942276@qq.com>
2023-08-29 15:21:30 +0800

    ui

--- Qt-IM/emoji.cpp
+++ Qt-IM/emoji.cpp
@@ -9,15 +9,16 @@ Emoji::Emoji(int px, int py, QWidget *parent) :
 
     ui->emojiTable->horizontalHeader()->setHidden(true);
     ui->emojiTable->verticalHeader()->setHidden(true);//删除表头表尾
-    for (int row = 0; row < 5; row++) {
-        ui->emojiTable->setRowHeight(row,50);
-    }
-    for (int col = 0; col < 5; col++) {
-        ui->emojiTable->setColumnWidth(col,50);
-    }
+
     ui->emojiTable->setFont(QFont("Arial", 25));
     ui->emojiTable->setStyleSheet("QTableWidget::item {text-align: center;}");
     ui->emojiTable->setEditTriggers(QAbstractItemView::NoEditTriggers);//单元格不可编辑
+
+
+    ui->emojiTable->resizeRowsToContents();
+    ui->emojiTable->resizeColumnsToContents();//单元格自适应大小
+
+
 
     this->setWindowFlags(Qt::FramelessWindowHint | Qt::Popup);
     this->move(px,py-this->height());
